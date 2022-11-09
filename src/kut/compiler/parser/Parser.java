b540b6b686bc822802f2a68dc368e79f5ff3136e
Hiroki NISHINO <nishino.hiroki@kochi-tech.ac.jp>
2022-11-09 16:28:13 +0900

    changed two function names. deleted an important comment.

--- src/kut/compiler/parser/Parser.java
+++ src/kut/compiler/parser/Parser.java
@@ -111,7 +111,6 @@ public class Parser
 	 */
 	public AstNode multiplicativeExpr() throws IOException, CompileErrorException
 	{
-		// primary | primary (* | / ) primary
 		AstNode lhs = primary();
 		
 		while(true) {
