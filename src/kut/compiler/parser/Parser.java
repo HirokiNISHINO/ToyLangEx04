d4eaeeba916a19146bfedcb2e747464b1dbecf39
Hiroki NISHINO <nishino.hiroki@kochi-tech.ac.jp>
2022-11-09 15:40:30 +0900

    Initiam Commit

---
+++ src/kut/compiler/parser/Parser.java
@@ -0,0 +1,186 @@
+package kut.compiler.parser;
+
+import java.io.IOException;
+
+import kut.compiler.exception.CompileErrorException;
+import kut.compiler.exception.SyntaxErrorException;
+import kut.compiler.lexer.Lexer;
+import kut.compiler.lexer.Token;
+import kut.compiler.lexer.TokenClass;
+import kut.compiler.parser.ast.AstNode;
+import kut.compiler.parser.ast.AstBinOp;
+import kut.compiler.parser.ast.AstIntLiteral;
+import kut.compiler.parser.ast.AstProgram;
+
+/**
+ * @author hnishino
+ *
+ */
+public class Parser 
+{
+	protected Lexer 	lexer;
+	protected Token		currentToken;
+	
+	/**
+	 * @param lexer
+	 */
+	public Parser(Lexer lexer) {
+		this.lexer = lexer;
+	}
+	
+	
+	
+	/**
+	 * @return
+	 */
+	protected Token getCurrentToken()
+	{
+		return this.currentToken;
+	}
+	
+	/**
+	 * 
+	 */
+	/**
+	 * 
+	 */
+	protected void consumeCurrentToken() throws IOException, CompileErrorException
+	{
+		this.currentToken = lexer.getNextToken();
+		return;
+	}
+	
+	/**
+	 * @return
+	 */
+	public AstNode parse() throws IOException, CompileErrorException
+	{
+		//read the first token.
+		consumeCurrentToken();
+		return program();
+	}
+	 
+
+	/**
+	 * @return
+	 */
+	protected AstNode program() throws IOException, CompileErrorException
+	{
+		AstNode node = expr();
+		
+		Token t = this.getCurrentToken();
+		if (t.getC() != TokenClass.EOF) {
+			throw new SyntaxErrorException("the program is not correctly terminated.");
+		}
+		
+		return new AstProgram(node);
+	}
+	
+	/**
+	 * @return
+	 * @throws IOException
+	 * @throws CompileErrorException
+	 */
+	public AstNode expr() throws IOException, CompileErrorException
+	{
+		AstNode lhs = term();
+		while(true) {
+			Token t = this.getCurrentToken();
+			
+			if (t == null) {
+				break;
+			}
+			
+			if (t.getC() != '+' && t.getC() != '-') {
+				break;
+			}
+			this.consumeCurrentToken();
+			
+			AstNode rhs = term();
+			AstNode binop = new AstBinOp(lhs, rhs, t);
+			lhs = binop;
+		}
+		return lhs;
+
+	}
+	
+	/**
+	 * @return
+	 * @throws IOException
+	 * @throws CompileErrorException
+	 */
+	public AstNode term() throws IOException, CompileErrorException
+	{
+
+		// primary | primary (* | / ) primary
+		AstNode lhs = primary();
+		
+		while(true) {
+			Token t = this.getCurrentToken();
+			
+			if (t == null) {
+				break;
+			}
+			
+			if (t.getC() != '*' && t.getC() != '/') {
+				break;
+			}
+			this.consumeCurrentToken();
+			
+			AstNode rhs = primary();
+			AstNode binop = new AstBinOp(lhs, rhs, t);
+			lhs = binop;
+		}
+		return lhs;
+	}
+	
+
+	/**
+	 * @return
+	 * @throws IOException
+	 * @throws CompileErrorException
+	 */
+	public AstNode primary() throws IOException, CompileErrorException
+	{	
+		//( expr )
+		Token t = this.getCurrentToken();
+		if (t.getC() == '(') {
+			this.consumeCurrentToken();
+			
+			AstNode e = expr();
+			
+			t = this.getCurrentToken();
+			if (t.getC() != ')') {
+				throw new SyntaxErrorException("expected ')' but found : " + t.getL() );
+			}
+			
+			this.consumeCurrentToken();
+			
+			return e;
+		}
+		
+		// if not expr.
+		return integer();
+	}
+	 
+	
+	/**
+	 * @return
+	 * @throws IOException
+	 * @throws CompileErrorException
+	 */
+	protected AstNode integer() throws IOException, CompileErrorException
+	{
+		Token t = this.getCurrentToken();
+		if (t.getC() != TokenClass.IntLiteral) {
+			throw new SyntaxErrorException("expected an integer literal, but found: " + t.getC());
+		}
+		
+		AstNode node = new AstIntLiteral(t);
+		this.consumeCurrentToken();
+		
+		return node;
+	}
+	
+	
+}
